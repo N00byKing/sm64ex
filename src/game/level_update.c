5e8aa4fc57e890ef8f6c32e593e947d3df2d1ce4
soopercool101 <soopercoolstages@gmail.com>
2026-01-18 21:47:21 -0600

    Use keyword FALSE

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -737,7 +737,7 @@ void initiate_painting_warp(void) {
                 }
 
                 if (!(warpNode.destLevel & 0x80)) {
-                    D_8032C9E0 = 0;
+                    D_8032C9E0 = FALSE;
                 }
 
                 initiate_warp(warpNode.destLevel & 0x7F, warpNode.destArea, warpNode.destNode, 0);
